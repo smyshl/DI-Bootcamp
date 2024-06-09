30551712a66db71bcbdfe87d1d75d9b37c0048d2
Eugene Smyshlyaev <e.smyshl@gmail.com>
2024-06-09 20:25:50 +0300

    Started working on task Week 2 Day 1 Exercises XP Gold

---
+++ Week_02/Day_1/W02_D1_Exercises_XP_Gold.py
@@ -0,0 +1,13 @@
+# Exercise 1 : Hello World-I love Python
+#     Print the following output in one line of code:
+# Hello world
+# Hello world
+# Hello world
+# Hello world
+# I love python
+# I love python
+# I love python
+# I love python
+
+print("\033[32mHello \033[35mworld\n" * 5, "\033[32m\bI \033[35mlove Python\n" * 5)
+print("\033[37m")
