4c04b1804e5bac494752c284d86ab70d184ebd52
Eugene Smyshlyaev <e.smyshl@gmail.com>
2024-06-10 17:59:40 +0300

    Finished task Week 2 Day 2 Daily Challenge

--- Week_02/Day_2/W02_D2_Daily_Challenge.py
+++ Week_02/Day_2/W02_D2_Daily_Challenge.py
@@ -12,3 +12,28 @@
 number, length = map(int, input("Please enter number and length of list separated with space: ").split(' '))
 
 print(f"Number - {number}, list of multiples with length of {length}: {[number * i for i in range (1, length + 1)]}")
+
+
+# Challenge 2
+#     Write a program that asks a string to the user, and display a new string with any duplicate consecutive letters removed.
+# Examples
+
+# user's word : "ppoeemm" ➞ "poem"
+# user's word : "wiiiinnnnd" ➞ "wind"
+# user's word : "ttiiitllleeee" ➞ "title"
+# user's word : "cccccaaarrrbbonnnnn" ➞ "carbon"
+
+# Notes
+# Final strings won’t include words with double letters (e.g. “passing”, “lottery”).
+
+input_string = input("\nPlease enter a word: ")
+char_list_without_duplicates = list(input_string)
+index = 0
+
+while index != len(char_list_without_duplicates) - 1:
+    if char_list_without_duplicates[index] == char_list_without_duplicates[index + 1]:
+        del char_list_without_duplicates[index + 1]
+    else:
+        index += 1
+
+print("Word without duplicates:", ''.join(char_list_without_duplicates))
