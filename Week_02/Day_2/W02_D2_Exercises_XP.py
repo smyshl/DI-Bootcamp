82d4783a04ca42079e11153a737b5773e94e15b9
Eugene Smyshlyaev <e.smyshl@gmail.com>
2024-06-10 15:44:39 +0300

    Finished task Week 2 Day 2 Excersises XP

--- Week_02/Day_2/W02_D2_Exercises_XP.py
+++ Week_02/Day_2/W02_D2_Exercises_XP.py
@@ -195,3 +195,31 @@ print("Who allowed to watch a movie: ", end='')
 print(*names_who_may_watch, sep=", ")
 
 
+# Exercise 10 : Sandwich Orders
+# Using the list below :
+
+# sandwich_orders = ["Tuna sandwich", "Pastrami sandwich", "Avocado sandwich", "Pastrami sandwich", "Egg sandwich", "Chicken sandwich", "Pastrami sandwich"]
+
+#     The deli has run out of pastrami, use a while loop to remove all occurrences of “Pastrami sandwich” from sandwich_orders.
+#     We need to prepare the orders of the clients:
+#         Create an empty list called finished_sandwiches.
+#         One by one, remove each sandwich from the sandwich_orders while adding them to the finished_sandwiches list.
+#     After all the sandwiches have been made, print a message listing each sandwich that was made, such as:
+
+# I made your tuna sandwich
+# I made your avocado sandwich
+# I made your egg sandwich
+# I made your chicken sandwich
+
+sandwich_orders = ["Tuna sandwich", "Pastrami sandwich", "Avocado sandwich", "Pastrami sandwich", "Egg sandwich", "Chicken sandwich", "Pastrami sandwich"]
+finished_sandwiches = []
+
+while "Pastrami sandwich" in sandwich_orders:
+    sandwich_orders.remove("Pastrami sandwich")
+
+while sandwich_orders:
+    finished_sandwiches.append(sandwich_orders[0])
+    del sandwich_orders[0]
+
+for sandwich in finished_sandwiches:
+    print("I made your", sandwich.lower())
