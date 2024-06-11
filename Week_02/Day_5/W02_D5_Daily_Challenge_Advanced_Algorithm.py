c493866d6976ce28691f7d0c6064fa06fc7f3570
Eugene Smyshlyaev <e.smyshl@gmail.com>
2024-06-11 16:56:54 +0300

    Started working on task Week 2 Day 3 Exercises XP

--- Week_02/Day_5/W02_D5_Daily_Challenge_Advanced_Algorithm.py
+++ Week_02/Day_5/W02_D5_Daily_Challenge_Advanced_Algorithm.py
@@ -13,6 +13,7 @@ import random
 list_of_numbers = [random.randint(0, 10000) for _ in range(20000)]
 target_number = 3728
 index = 1
+list_of_pairs = []
 
 list_of_numbers.sort()
 
@@ -29,7 +30,21 @@ else:
     target_index = list_of_numbers.index(closest_number)
     print(closest_number, target_index, [list_of_numbers[target_index + i] for i in range(1, 11)])
 
-    
+new_list_of_numbers = list_of_numbers[:target_index + 1]
+print(new_list_of_numbers[:5], new_list_of_numbers[-5:])
+
+while len(new_list_of_numbers) >=2:
+
+    while new_list_of_numbers[-1] + new_list_of_numbers[0] < target_number:
+    # print(new_list_of_numbers[0])
+        del new_list_of_numbers[0]
+    # break
+    print(new_list_of_numbers[0], "+", new_list_of_numbers[-1], "=", new_list_of_numbers[-1] + new_list_of_numbers[0])
+
+    list_of_pairs.append([new_list_of_numbers[-1], new_list_of_numbers[0]])
+    del new_list_of_numbers[-1]
+    del new_list_of_numbers[0]
+    print(new_list_of_numbers[:5], new_list_of_numbers[-5:])
 
 
 # print(list_of_numbers)
