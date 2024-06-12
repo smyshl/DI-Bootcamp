5a2c4882f9a85d06887cbc6c64a6279f1ce7ee25
Eugene Smyshlyaev <e.smyshl@gmail.com>
2024-06-12 14:25:19 +0300

    Started working on task Week 2 Day 3 Daily Challenge Dictionaries

---
+++ Week_02/Day_3/W02_D3_Daily_Challenge_Dictionaries.py
@@ -0,0 +1,18 @@
+# Challenge 1
+#     Ask a user for a word
+#     Write a program that creates a dictionary. This dictionary stores the indexes of each letter in a list.
+#         Make sure the letters are the keys.
+#         Make sure the letters are strings.
+#         Make sure the indexes are stored in a list and those lists are values.
+
+# Examples
+# "dodo" ➞ { "d": [0, 2], "o": [1, 3] }
+# "froggy" ➞ { "f":  [0], "r": [1], "o": [2], "g": [3, 4], "y": [5] }
+# "grapes" ➞ { "g": [0], "r": [1], "a": [2], "p": [3]}
+print("--------------------Exercise 3----------------------")
+
+
+
+
+
+input("Please press enter to go to the next exercise")
