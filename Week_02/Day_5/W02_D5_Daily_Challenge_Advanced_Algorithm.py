8d83a7fa09d3bf8ddb7037aeb48062efd23fe732
Eugene Smyshlyaev <e.smyshl@gmail.com>
2024-06-12 23:59:09 +0300

    Finished task Week 2 Day 5 Daily Challenge Advanced Algorithm

--- Week_02/Day_5/W02_D5_Daily_Challenge_Advanced_Algorithm.py
+++ Week_02/Day_5/W02_D5_Daily_Challenge_Advanced_Algorithm.py
@@ -15,11 +15,19 @@ target_number = 3728
 index = 1
 list_of_pairs = []
 
+print("Length of the given list:", len(list_of_numbers))
+
+# Forming a new list of numbers which contains only numbers <= target_number
+# Sorting in ascending order
+# Deleting duplicate numbers
+
+list_of_numbers = list(set(list_of_numbers))
 list_of_numbers.sort()
+
+print("Length of the list after deleting duplicate numbers:", len(list_of_numbers))
 
 if target_number in list_of_numbers:
     target_index = list_of_numbers.index(target_number)
-    print(target_index, list_of_numbers[target_index])
 else:
     while target_number - index not in list_of_numbers:
         index += 1
@@ -28,23 +36,25 @@ else:
             break
     closest_number = target_number - index
     target_index = list_of_numbers.index(closest_number)
-    print(closest_number, target_index, [list_of_numbers[target_index + i] for i in range(1, 11)])
 
 new_list_of_numbers = list_of_numbers[:target_index + 1]
-print(new_list_of_numbers[:5], new_list_of_numbers[-5:])
+print(f"Length of the list after deleting numbers > {target_number} (target_number):", len(new_list_of_numbers))
 
-while len(new_list_of_numbers) >=2:
-
-    while new_list_of_numbers[-1] + new_list_of_numbers[0] < target_number:
-    # print(new_list_of_numbers[0])
+while len(new_list_of_numbers) >= 2:
+    if new_list_of_numbers[-1] + new_list_of_numbers[0] == target_number:
+        # if sum of first and last element equal target_number
+        # add this pair to the list_of_pairs
+        # delete first and last element from new_list_of_numbers
+        list_of_pairs.append([new_list_of_numbers[0], new_list_of_numbers[-1]])
         del new_list_of_numbers[0]
-    # break
-    print(new_list_of_numbers[0], "+", new_list_of_numbers[-1], "=", new_list_of_numbers[-1] + new_list_of_numbers[0])
+        del new_list_of_numbers[-1]
+    elif new_list_of_numbers[-1] + new_list_of_numbers[0] > target_number:
+        # if this condition is True, it means that the bigger number should be deleted
+        del new_list_of_numbers[-1]
+    else:
+        # if this condition is True (new_list_of_numbers[-1] + number < target_number),
+        # it means that the smaller number should be deleted
+        # stop looping
+        del new_list_of_numbers[0]
 
-    list_of_pairs.append([new_list_of_numbers[-1], new_list_of_numbers[0]])
-    del new_list_of_numbers[-1]
-    del new_list_of_numbers[0]
-    print(new_list_of_numbers[:5], new_list_of_numbers[-5:])
-
-
-# print(list_of_numbers)
+print("Pairs of numbers found:", len(list_of_pairs))
