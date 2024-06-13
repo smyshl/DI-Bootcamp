376bccd76f7c24d0334e88e14e143cf0c9b945e8
Eugene Smyshlyaev <e.smyshl@gmail.com>
2024-06-13 20:06:47 +0300

    Started working on task Week 2 Day 4 Timed Challenge #1

---
+++ Week_02/Day_4/W02_D4_Timed_Challenge_no_1.py
@@ -0,0 +1,12 @@
+# Count occurence
+
+# Write a program which takes a string and a character as an input, and finds out the number of occurrences the character has in the string.
+
+# String: Programming is cool!
+# Character: o
+# 3
+
+# String: This is a great example
+# Character: y
+# 0
+
