34d4c82c6a6d3f8bde6db2ac0ebad057e2b8cf54
Eugene Smyshlyaev <e.smyshl@gmail.com>
2024-06-14 16:53:03 +0300

    Continue working on task Week 2 Day 5 Mini-Project - Tic Tac Toe

--- Week_02/Day_5/W02_D5_Mini_Project_Tic_Tac_Toe.py
+++ Week_02/Day_5/W02_D5_Mini_Project_Tic_Tac_Toe.py
@@ -25,3 +25,100 @@
 #     After contemplating the question above, think about splitting your code into smaller pieces (functions).
 #     Remember to follow the single responsibility principle! each function should do one thing and do it well!
 
+def print_board(list_of_turns=["0", "X", "0", "X", "0", "X", "0", "X", "0"]):
+    '''Print a TIC TAC TOE board with turns given in the list_of_turns'''
+
+    print()
+    print("     -TIC-TAC-TOE-")
+    print("     -------------")
+    print(f"   3 | {' | '.join(list_of_turns[-3:])} |")
+    print("     -------------")
+    print(f"   2 | {' | '.join(list_of_turns[-6:-3])} |")
+    print("     -------------")
+    print(f"   1 | {' | '.join(list_of_turns[:-6])} |")
+    print("     -------------")
+    print("       a   b   c  \n")
+
+
+def check_whos_turn(list_of_turns):
+    '''Checks which player's turn'''
+    '''Returns "X", "0" or "GAME OVER"'''
+
+    if not ("X" or "0") in list_of_turns:
+        return "X"
+    elif list_of_turns.count(" ") == 0:
+        return "GAME OVER"
+    elif list_of_turns.count("X") > list_of_turns.count("0"):
+        return "0"
+    elif list_of_turns.count("X") <= list_of_turns.count("0"):
+        return "X"
+
+
+def input_turn(list_of_turns, whos_turn):
+    '''Making input of turn, check if it is correct'''
+    '''Returns updated list_of_turns'''
+
+    col_index_dict = {"a": 0, "b": 1, "c": 2}
+    row_index_dict = {"1": 0, "2": 1, "3": 2}
+
+    column = input("Enter column (a, b, c): ")
+    row = input("Enter row (1, 2, 3): ")
+
+    new_turn_index = col_index_dict[column] + row_index_dict[row] * 3
+
+    if list_of_turns[new_turn_index] == "X" or list_of_turns[new_turn_index] == "0":
+        print("This move has already done, try another")
+    else:
+        list_of_turns[new_turn_index] = whos_turn
+
+
+def check_win(list_of_turns):
+    '''Checks the win'''
+    '''Returns "X", "0" or "tie"'''
+
+    for i in range(3):
+        if list_of_turns[i * 3] == list_of_turns[i * 3 + 1] == list_of_turns[i * 3 + 2]:
+            return list_of_turns[i * 3]
+        if list_of_turns[i] == list_of_turns[i + 3] == list_of_turns[i + 6]:
+            return list_of_turns[i]
+        
+
+
+
+
+def make_turns(list_of_turns=[" "]*9):
+
+    # whos_turn = check_whos_turn(list_of_turns)
+
+    while check_whos_turn(list_of_turns) != "GAME OVER":
+        print(f"Player's {check_whos_turn(list_of_turns)} turn...")
+        left_turns = list_of_turns.count(" ")  # for checking if the input was successfull
+        # print("left_turns", left_turns)
+        input_turn(list_of_turns, check_whos_turn(list_of_turns))
+        # print("turns left after input", list_of_turns.count(" "))
+
+        while left_turns == list_of_turns.count(" "): # checking if the input was successfull
+            input_turn(list_of_turns, check_whos_turn(list_of_turns))
+
+        print_board(list_of_turns)
+
+    
+    print(check_whos_turn(list_of_turns))
+
+
+
+# list_of_turns = ["1", "2", "X", "4", "5", "X", "7", "8", "X"]
+list_of_turns = [" "]*9
+
+
+# print_board()
+# input("Press Enter to start game")
+print_board(list_of_turns)
+make_turns(list_of_turns)
+print(check_win(list_of_turns))
+
+
+
+
+
+
