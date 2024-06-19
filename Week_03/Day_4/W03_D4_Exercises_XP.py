e1cb74633ad8452249fb2ca46a4fd51855bf1459
Eugene Smyshlyaev <e.smyshl@gmail.com>
2024-06-19 19:34:52 +0300

    Finished task Week 3 Day 4 Exercises XP

--- Week_03/Day_4/W03_D4_Exercises_XP.py
+++ Week_03/Day_4/W03_D4_Exercises_XP.py
@@ -1,4 +1,4 @@
-import random
+import random, json
 
 # Exercise 1 – Random Sentence Generator
 
@@ -27,16 +27,16 @@ def get_words_from_file(filename:str) -> list:
         file_content = file.read()
 
     words_list = file_content.split("\n")
-    
     return words_list
 
-def get_random_sentence(words_list:list, length:int=10) -> str:
+
+def get_random_sentence(words_list:list, length:int) -> str:
     
     sentence_list = random.choices(words_list, k=length)
     sentence = (" ".join(sentence_list) + ".").lower()
     sentence = sentence[0].upper() + sentence[1:]
-
     return sentence
+
 
 def input_sentence_length() -> int:
 
@@ -55,6 +55,9 @@ def input_sentence_length() -> int:
 
 def exercise_1():
     print("--------------------Exercise 1----------------------")
+    print("This program reads the words from the file to a list, asks a user a number,")
+    print("and then create and print a sentence using words from the list.")
+    print("It uses as many words as the number entered by the user.")
 
     source_file = "sowpods.txt"
     words_list = get_words_from_file(source_file)
@@ -63,10 +66,57 @@ def exercise_1():
     print(sentence)
 
 
+# Exercise 2: Working with JSON
+
+# import json
+# sampleJson = """{ 
+#    "company":{ 
+#       "employee":{ 
+#          "name":"emma",
+#          "payable":{ 
+#             "salary":7000,
+#             "bonus":800
+#          }
+#       }
+#    }
+# }"""
+#     Access the nested “salary” key from the JSON-string above.
+#     Add a key called “birth_date” to the JSON-string at the same level as the “name” key.
+#     Save the dictionary as JSON to a file.
+
+def add_birthday(json_string:str, birth_date:str):
+
+    name_index = json_string.index("name")
+    start_index = json_string[name_index:].index(",") + name_index + 1
+    new_json_string = json_string[:start_index] + f' "birth_date":"{birth_date}", ' + json_string[start_index + 1:]
+
+    return new_json_string
+
+
+def json_file_write(json_string:str, file_name:str):
+
+    json_dict = json.loads(json_string)  
+    with open(file_name, mode="w") as json_file:
+        json.dump(json_dict, json_file)
+
+
+def exercise_2():
+    print("--------------------Exercise 2----------------------")
+
+    sampleJson = """{"company":{"employee":{"name":"emma", "payable":{"salary":7000, "bonus":800}}}}"""
+    sample_json_dict = json.loads(sampleJson)
+    print("salary:", sample_json_dict['company']['employee']['payable']['salary'])
+ 
+    new_sampleJson = add_birthday(sampleJson, "2001.07.15")
+
+    json_file = "sample_json.json"
+    json_file_write(new_sampleJson, json_file)
+
+
 def main():
     
     exercise_1()
-    # exercise_2()
+    exercise_2()
 
 
 if __name__ == "__main__":
