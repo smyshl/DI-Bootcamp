c99327bc16bfaf6d3437c2dfbb5b3403bb4fb219
Eugene Smyshlyaev <e.smyshl@gmail.com>
2024-06-23 16:15:08 +0300

    Finished task Week 4 Day 1 Exercise XP +

---
+++ Week_04/Day_1/W04_D1_Exercise_XP_plus.sql
@@ -0,0 +1,69 @@
+CREATE TABLE students (
+	id serial PRIMARY KEY,
+	last_name VARCHAR(20),
+	first_name VARCHAR(15),
+	birth_date date
+);
+
+INSERT INTO students (first_name, last_name, birth_date)
+	VALUES (
+	'Marc',
+	'Benichou',
+	'1998-11-02'
+	),
+	(
+	'Yoan',
+	'Cohen',
+	'2010-12-03'
+	),
+	(
+	'Lea',
+	'Benichou',
+	'1987-07-27'
+	),
+	(
+	'Amelia',
+	'Dux',
+	'1996-04-07'
+	),
+	(
+	'David',
+	'Grez',
+	'2003-06-04'
+	),
+	(
+	'Omer',
+	'Simpson',
+	'1980-10-03'
+	-- );
+
+SELECT * FROM students;
+
+SELECT first_name, last_name FROM students;
+
+SELECT first_name, last_name FROM students
+	WHERE id = 2;
+
+SELECT first_name, last_name FROM students
+	WHERE last_name = 'Benichou' and first_name = 'Marc';
+
+SELECT first_name, last_name FROM students
+	WHERE last_name = 'Benichou' or first_name = 'Marc';
+
+SELECT first_name, last_name FROM students
+	WHERE first_name ILIKE '%a%';
+
+SELECT first_name, last_name FROM students
+	WHERE first_name ILIKE 'a%';
+
+SELECT first_name, last_name FROM students
+	WHERE first_name ILIKE '%a';
+
+SELECT first_name, last_name FROM students
+	WHERE first_name ILIKE '%a_';
+
+SELECT first_name, last_name FROM students
+	WHERE id = 1 OR id = 3;
+
+SELECT * FROM students
+	WHERE birth_date >= '2000-01-01';
