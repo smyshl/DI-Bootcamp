a793b27c0a6e94cdefaa3d44205a5d0a7f6e35f8
Eugene Smyshlyaev <e.smyshl@gmail.com>
2024-07-01 18:21:30 +0300

    Continue working on task Week 05 Day 2 Exercises XP

--- Week_05/Day_2/W05_D2_Exercises_XP.js
+++ Week_05/Day_2/W05_D2_Exercises_XP.js
@@ -68,3 +68,25 @@ for (person of people){
 }
 
 
+/*
+Exercise 2 : Your favorite colors
+Instructions
+    Create an array called colors where the value is a list of your five favorite colors.
+    Loop through the array and as you loop console.log a string like so: “My #1 choice is blue”, “My #2 choice is red” ect… .
+    Bonus: Change it to console.log “My 1st choice”, “My 2nd choice”, “My 3rd choice”, picking the correct suffix for each number.
+    Hint : create an array of suffixes to do the Bonus
+*/
+
+const colors = ["green", "blue", "red", "orange", "black", "white"];
+
+for (index in colors){
+    ind_for_print = +index + 1
+    console.log("My #"+ ind_for_print + " choice is", colors[index]);
+};
+
+const suffix_arr = ["st", "nd", "rd", "th", "th", "th"]
+
+for (index in colors){
+    ind_for_print = +index + 1 + suffix_arr[index]
+    console.log("My " + ind_for_print + " choice is", colors[index]);
+};
