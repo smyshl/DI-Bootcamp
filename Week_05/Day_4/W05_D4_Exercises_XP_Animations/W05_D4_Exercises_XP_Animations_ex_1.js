7a2b0ac87407a6fba7126d670f9d822071669fec
Eugene Smyshlyaev <e.smyshl@gmail.com>
2024-07-04 16:09:10 +0300

    Continue working on task Week 5 Day 4 Exercises XP Animations

--- Week_05/Day_4/W05_D4_Exercises_XP_Animations/W05_D4_Exercises_XP_Animations_ex_1.js
+++ Week_05/Day_4/W05_D4_Exercises_XP_Animations/W05_D4_Exercises_XP_Animations_ex_1.js
@@ -33,4 +33,39 @@ Part III
     Instead of clicking on the button, the interval will be cleared (ie. clearInterval) as soon as there will be 5 paragraphs inside the <div id="container">.
 */
 
-// 1.1
+// 1
+function alert_hello_world(){
+    alert("Hello World")
+};
+
+setTimeout(alert_hello_world, 5000);
+
+// 2
+function add_paragraph(){
+    let _target_place = document.getElementById("container");
+    let _p = document.createElement("p");
+    _p.textContent = "Hello World"
+    _target_place.appendChild(_p)
+};
+
+setTimeout(add_paragraph, 2000)
+
+// 3
+let _interval_timer = setInterval(add_paragraph, 2000);
+
+let _check_paragraph = setInterval(function (){
+    if (document.getElementsByTagName("p").length == 5){
+        stop_adding();
+    };
+console.log("checking paragraphs");
+}, 1000)
+
+function stop_adding(){
+    clearInterval(_interval_timer);
+    clearInterval(_check_paragraph);
+};
+
+let _button = document.getElementById("clear");
+_button.addEventListener("click", stop_adding);
+
+
