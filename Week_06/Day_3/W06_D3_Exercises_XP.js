f7f2da3e08c1d05851ac47d52cab13755ad0a691
Eugene Smyshlyaev <e.smyshl@gmail.com>
2024-07-09 16:35:07 +0300

    Finished task Week 6 Day 3 Exercises XP

---
+++ Week_06/Day_3/W06_D3_Exercises_XP.js
@@ -0,0 +1,194 @@
+/*
+Exercise 1 : Location
+Instructions
+
+    Analyze the code below. What will be the output?
+
+const person = {
+    name: 'John Doe',
+    age: 25,
+    location: {
+        country: 'Canada',
+        city: 'Vancouver',
+        coordinates: [49.2827, -123.1207]
+    }
+}
+
+const {name, location: {country, city, coordinates: [lat, lng]}} = person;
+console.log(`I am ${name} from ${city}, ${country}. Latitude(${lat}), Longitude(${lng})`);
+*/
+
+// Answer: The output will be: I am from Vancouver, Canada. Latitude(49.2827), Longitude(-123.1207)
+// Check: Yes
+
+
+/*
+Exercise 2: Display Student Info
+Instructions
+
+function displayStudentInfo(objUser){
+    //destructuring
+}
+
+displayStudentInfo({first: 'Elie', last:'Schoppik'});
+    Using the code above, destructure the parameter inside the function and return a string as the example seen below:
+    //output : 'Your full name is Elie Schoppik'
+*/
+
+function displayStudentInfo(objUser){
+    let {first, last} = objUser;
+    return `Your full name is ${first} ${last}`
+}
+
+console.log(displayStudentInfo({first: 'Elie', last:'Schoppik'}));
+
+
+/*
+Exercise 3: User & id
+Instructions
+
+Using this object const users = { user1: 18273, user2: 92833, user3: 90315 }
+
+    Using methods taught in class, turn the users object into an array:
+    Excepted output: [ [ 'user1', 18273 ], [ 'user2', 92833 ], [ 'user3', 90315 ] ]
+    FYI : The number is their ID number.
+
+    Modify the outcome of part 1, by multipling the user’s ID by 2.
+    Excepted output: [ [ 'user1', 36546 ], [ 'user2', 185666 ], [ 'user3', 180630 ] ]
+*/
+
+const users = { user1: 18273, user2: 92833, user3: 90315 };
+
+let users_array = Object.entries(users);
+
+users_array.forEach((element) => element[1] *= 2)
+
+console.log(users_array);
+
+
+/*
+Exercise 4 : Person class
+Instructions
+
+    Analyze the code below. What will be the output?
+
+class Person {
+  constructor(name) {
+    this.name = name;
+  }
+}
+
+const member = new Person('John');
+console.log(typeof member);
+*/
+
+// Answer: The output will be: oblect
+// Check: Yes
+
+
+/*
+Exercise 5 : Dog class
+Instructions
+
+Using the Dog class below:
+
+class Dog {
+  constructor(name) {
+    this.name = name;
+  }
+};
+    Analyze the options below. Which constructor will successfully extend the Dog class?
+  // 1
+class Labrador extends Dog {
+  constructor(name, size) {
+    this.size = size;
+  }
+};
+  // 2
+class Labrador extends Dog {
+  constructor(name, size) {
+    super(name);
+    this.size = size;
+  }
+};
+  // 3
+class Labrador extends Dog {
+  constructor(size) {
+    super(name);
+    this.size = size;
+  }
+};
+  // 4
+class Labrador extends Dog {
+  constructor(name, size) {
+    this.name = name;
+    this.size = size;
+  }
+};
+*/
+
+// Answer: the second one is written correctly
+
+
+/*
+Exercise 6 : Challenges
+
+    Evaluate these (ie True or False)
+
+    [2] === [2] 
+    {} === {}
+
+
+    What is, for each object below, the value of the property number and why?
+
+    const object1 = { number: 5 }; 
+    const object2 = object1; 
+    const object3 = object2; 
+    const object4 = { number: 5};
+
+    object1.number = 4;
+    console.log(object2.number)
+    console.log(object3.number)
+    console.log(object4.number)
+
+    Create a class Animal with the attributes name, type and color. The type is the animal type, for example: dog, cat, dolphin ect …
+    Create a class Mamal that extends from the Animal class. Inside the class, add a method called sound(). This method takes a parameter: the sound the animal makes, and returns the details of the animal (name, type and color) as well as the sound it makes.
+    Create a farmerCow object that is an instance of the class Mamal. The object accepts a name, a type and a color and calls the sound method that “moos” her information.
+    For example: Moooo I'm a cow, named Lily and I'm brown and white
+*/
+
+console.log([2] === [2]);   // false
+console.log({} === {});     // false
+
+
+const object1 = { number: 5 }; 
+const object2 = object1; 
+const object3 = object2; 
+const object4 = { number: 5};
+
+object1.number = 4;
+console.log(object2.number);    // 4
+console.log(object3.number);    // 4
+console.log(object4.number);    // 5
+
+
+class Animal {
+    constructor (name, type, color) {
+        this.name = name;
+        this.type = type;
+        this.color = color;
+    }
+}
+
+class Mamal extends Animal {
+    constructor (name, color, _sound) {
+        super(name, "Mamal", color);
+        this._sound = _sound;
+    }
+    sound() {
+        return `${this._sound} I'm a cow, named ${this.name} and I'm ${this.color}`;
+    }
+}
+
+let farmerCow = new Mamal("Lily", "brown and white", "Moooo")
+console.log(farmerCow.sound());
