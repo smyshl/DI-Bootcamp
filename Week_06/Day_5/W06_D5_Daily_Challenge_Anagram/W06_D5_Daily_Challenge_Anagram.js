4d4d4f5c6415c957301f52818d1a59e6e63dc4bd
Eugene Smyshlyaev <e.smyshl@gmail.com>
2024-07-13 21:06:09 +0300

    Finished task Week 6 Day 5 Daily Challenge Anagram

--- Week_06/Day_5/W06_D5_Daily_Challenge_Anagram/W06_D5_Daily_Challenge_Anagram.js
+++ Week_06/Day_5/W06_D5_Daily_Challenge_Anagram/W06_D5_Daily_Challenge_Anagram.js
@@ -15,4 +15,27 @@ Some Help
     Trim whitespace prior to comparison.
 */
 
+function delete_spaces(_string){
+    // returns sorted string without spaces
+    let _string_arr = [... _string.trim()];
+    _string_arr.sort();
+    return _string_arr.slice(_string_arr.lastIndexOf(' ') + 1,).join('');
+}
+
+
+function is_anagram(string_1, string_2) {
+    let string_1_arr = [... delete_spaces(string_1.toLowerCase())];
+    let string_2_arr = [... delete_spaces(string_2.toLowerCase())];
+
+    if (string_1_arr.join('') === string_2_arr.join('')) {
+        return true;
+    } else {
+        return false;
+    };
+}
+
+let _string_1 = 'The Morse Code';
+let _string_2 = 'Here come dots'
+
+console.log('"' + _string_1 + '" is anafram "' + _string_2 + '"?', '-', is_anagram(_string_1, _string_2));
 
