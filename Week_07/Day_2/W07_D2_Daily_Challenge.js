4a8644f639213dc59427d4a09af7813b8193f250
Eugene Smyshlyaev <e.smyshl@gmail.com>
2024-07-16 20:29:08 +0300

    Started working on task Week 7 Day 2 Daily Challenge

---
+++ Week_07/Day_2/W07_D2_Daily_Challenge.js
@@ -0,0 +1,9 @@
+/*Instructions
+    Use this Giphy API Random documentation. Use this API Key : hpvZycW22qCjn5cRM1xtWB8NKq4dQ2My
+    In the HTML file, add a form, containing an input and a button. This input is used to fetch gif depending on a specific category.
+    In the JS file, create a program to fetch one random gif depending on the search of the user (ie. If the search is “sun”, append on the page one gif with a category of “sun”).
+    The gif should be appended with a DELETE button next to it. Hint : to find the URL of the gif, look for the sub-object named “images” inside the data you receive from the API.
+    Allow the user to delete a specific gif by clicking the DELETE button.
+    Allow the user to remove all of the GIFs by clicking a DELETE ALL button.
+*/
+
