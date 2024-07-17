336b0cdeff8fc25590c433636816902b3e51ff6c
Eugene Smyshlyaev <e.smyshl@gmail.com>
2024-07-17 16:39:05 +0300

    Continue working on task Week 7 Day 3 Daily Challenge Currency Converter

--- Week_07/Day_3/W07_D3_Daily_Challenge_Currency/W07_D3_Daily_Challenge_Currency.js
+++ Week_07/Day_3/W07_D3_Daily_Challenge_Currency/W07_D3_Daily_Challenge_Currency.js
@@ -14,3 +14,58 @@ Note
     Example : if the conversion was from EUR to GBP, as soon as the button is clicked on, the conversion should be from GBP to EUR.
 */
 
+let currencies_url = 'https://v6.exchangerate-api.com/v6/349cf1b6381ea6b5091d8832/codes';
+
+let first_currency_selector = document.getElementById('first_currency');
+let target_currency_selector = document.getElementById('target_currency');
+
+
+function create_new_option(_arr){
+    let _new_option = document.createElement('option')
+    _new_option.value = _arr[0];
+    _new_option.textContent = _arr[1];
+    return _new_option;
+}
+
+
+function selectors_render(_currencies_array){
+    _currencies_array.forEach(currency => {
+        first_currency_selector.appendChild(create_new_option(currency));
+        target_currency_selector.appendChild(create_new_option(currency));
+});
+}
+
+
+function switch_currencies(e){
+    e.preventDefault();
+    [first_currency_selector.value, target_currency_selector.value] = [target_currency_selector.value, first_currency_selector.value]
+}
+
+
+async function get_data_from_api(_url, currencies = true){
+
+    try {       // trying to fetch character data from API
+        let response = await fetch (_url);
+        // console.log(response);
+        if (response.ok) {
+            let data = await response.json();
+
+            if (currencies) {
+                selectors_render(data.supported_codes);
+            } else {
+
+
+            }
+
+
+
+        } else {
+
+            throw new Error ("Response character data NOT ok")
+        }
+    } catch (error) {
+        console.log(error);
+    };
+};
+
+get_data_from_api(currencies_url);
