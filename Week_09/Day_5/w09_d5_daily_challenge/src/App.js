4f6230b03ad463d247ec4989750a22b296a0ebd8
Eugene Smyshlyaev <e.smyshl@gmail.com>
2024-08-03 17:57:46 +0300

    Continue working on task Week 9 Day 5 Daily Challenge

--- Week_09/Day_5/w09_d5_daily_challenge/src/App.js
+++ Week_09/Day_5/w09_d5_daily_challenge/src/App.js
@@ -1,18 +1,12 @@
-import { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [ num_1, setNum_1] = useState(0);
-  const [ num_2, setNum_2] = useState(0);
 
   const calculateButtonClick = (e) => {
     e.preventDefault();
-    console.log(e.target.value);
-    
-    // if (isNaN(Number(e.target.value))) {
-    //   alert("Please enter a number");
-    //   e.target.value = null;
-    // }
+
+    e.target[4].value = Number(e.target[0].value) + Number(e.target[2].value)
+ 
   }
 
 
@@ -21,11 +15,13 @@ function App() {
 
         <div>
       <h1>Calculator</h1>
-      <form>
+      <form onSubmit={calculateButtonClick}>
       <input id='num_1' type='number'></input>
-      <select></select>
+      <select name='operation' id='operation_select' required>
+        <option value="add" selected>+</option>
+      </select>
       <input id='num_2' type='number'></input><br />
-      <button onClick={calculateButtonClick}>Calculate</button><br />
+      <input type='submit' value={"Calculate"}></input><br />
       <input id='result' disabled></input>
       </form>
         </div>
