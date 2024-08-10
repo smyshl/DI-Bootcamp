68a92ac565d0da09cf8177018d279c64e7142313
Eugene Smyshlyaev <e.smyshl@gmail.com>
2024-08-10 18:00:15 +0300

    Finished task Week 9 Day 5 Mini Project Random Quote Gen

--- Week_09/Day_5/w09_d5_mini_project_random_quote_gen/src/App.js
+++ Week_09/Day_5/w09_d5_mini_project_random_quote_gen/src/App.js
@@ -1,64 +1,52 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import quotes from './QuotesDatabase.js';
 import './App.css';
 
-// for (let i = 0; i < quotes.length; i++) {
-//   console.log(i, quotes[i].quote);
-  
-// }
 
-let counter = 0;
-let buttonClicks = 0;
 let quotesToDisplay = [...quotes];
-// console.log(quotesToDisplay);
 
 
 function getRandomQuote () {
-
-  counter++
-
-  
-    let random_index = Math.floor(Math.random() * (quotesToDisplay.length + 1));
-
-  // console.log("getRandomQuote quotes.length", quotesToDisplay.length, random_index, counter);
-    console.log("index:", random_index, "length:", quotesToDisplay.length);
+    let random_index = Math.floor(Math.random() * quotesToDisplay.length);
     let [random_quote] = quotesToDisplay.splice(random_index, 1);
-    console.log(random_quote);
     
-
+  if (quotesToDisplay.length === 0) quotesToDisplay = [...quotes];
     
     return random_quote;
 };
 
 
+function getRandomColor () {
+  const red = Math.floor(Math.random() * 257);
+  const green = Math.floor(Math.random() * 257);
+  const blue = Math.floor(Math.random() * 257);
+  return `rgb(${red} ${green} ${blue})`;
+};
+
+
 function App() {
 
-  const [quote, setQuote] = useState({});
-
-  // console.log(quote.quote, quotes.length);
+  const [ quote, setQuote ] = useState({});
+  const [ color, setColor ] = useState('rgb(210 105 30)')
 
   useEffect (() => {
     setQuote(getRandomQuote());
   }, [])
 
-
   const onButtonClick = () => {
-    buttonClicks++
     const new_quote = getRandomQuote();
-    setQuote(new_quote);    
-  // console.log(`Button clicks: ${buttonClicks}. RandomQuotes calls num: ${counter}`);    
-
+    setQuote(new_quote);
+    setColor(getRandomColor());
   };
   
   return (
     
-    <div id="main_wrapper">
+    <div id="main_wrapper" style={{backgroundColor: color}}>
 
       <section>
-        <h1>"{quote.quote}"</h1>
-        <h5>-{quote.author}-</h5>
-        <p>{quotesToDisplay.length}</p>
-        <button onClick={onButtonClick}>New quote</button>
+        <h1 style={{color: color}}>"{quote.quote}"</h1>
+        <h5 style={{color: color}}>-{quote.author}-</h5>
+        <button onClick={onButtonClick} style={{backgroundColor: color}}>New quote</button>
       </section>
 
     </div>
