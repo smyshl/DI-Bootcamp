ce3eb44ba859113e18bff52d1c0235c98891b63a
Eugene Smyshlyaev <e.smyshl@gmail.com>
2024-08-12 16:56:05 +0300

    Finished task Week 9 Day 5 Daily Challenge

--- Week_09/Day_5/w09_d5_daily_challenge/src/App.js
+++ Week_09/Day_5/w09_d5_daily_challenge/src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import './App.css';
 
 function App() {
@@ -7,27 +7,45 @@ function App() {
   const [num_2, setNum_2] = useState(0);
   const [result, setResult] = useState(0);
 
+  let selectRef = useRef();
+
   const calculateButtonClick = (e) => {
     e.preventDefault();
 
-    setResult(num_1 + num_2);
- 
+    let operand = selectRef.current.value
+
+    if (operand === 'add') setResult(num_1 + num_2);
+    if (operand === 'minus') setResult(num_1 - num_2);
+    if (operand === 'multiply') setResult(num_1 * num_2);
+    if (operand === 'divide') {
+      if (num_2 != 0) {
+        setResult(num_1 / num_2);
+      } else alert("You can't divide by zero")
+    }; 
   }
 
 
   return (
     <div id='main_wrapper'>
 
-        <div onSubmit={calculateButtonClick}>
+        <div id='calculator'>
       <h1>Calculator</h1>
-      <form>
+      <form onSubmit={calculateButtonClick}>
       <input id='num_1' type='number' value={num_1} onChange={(e) => setNum_1(Number(e.target.value))}></input>
-      <select name='operation' id='operation_select' required>
-        <option value="add" selected>+</option>
+      <select name='operation' id='operation_select' required ref={selectRef}>
+        <option value="add"> + </option>
+        <option value="minus"> - </option>
+        <option value="multiply"> * </option>
+        <option value="divide"> / </option>
       </select>
       <input id='num_2' type='number' value={num_2} onChange={(e) => setNum_2(Number(e.target.value))}></input><br />
-      <input type='submit' value={"Calculate"}></input><br />
-      <input id='result' disabled value={result}></input>
+      <div>
+        <input id='button' type='submit' value={"Calculate"}></input><br />
+      </div>
+      <div>
+        <input id='result' disabled value={result}></input>
+      </div>
+      
       </form>
         </div>
 
